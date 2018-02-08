a5bacd595c3b3bd71b84103ca3fc481906811e4f
riyaden <riyaden@yahoo.co.id>
2018-02-08 18:04:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+		</div>
+		 <div class="copywrite">
+		 	<p class="cmpny1"></p><br>
+			 <p id="link" class="text-center">
+			 	<a href="">About Us</a> | 
+			 	<a href="">Site Map</a> | 
+			 	<a href="">Privacy Policy</a> | 
+			 	<a href="">Contact Us</a> | 2018 Datalogic Software
+			 </p>
+		 </div>
+	 </div>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Datalogic Software & Sentracare Application</title>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+	<link href="css/style.css" rel='stylesheet' type='text/css'/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content=""/>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="col-sm-3 col-md-2 sidebar">
+	<div class="sidebar_top">
+		<div class="row">
+            <div class="input-group col-md-12">
+                <input type="text" class="form-control input-sm" placeholder="Search . .">
+                <span class="input-group-btn">
+                    <button class="btn btn-info btn-sm" type="button">
+                    	<i class="glyphicon glyphicon-search"></i>
+                	</button>
+                </span>
+            </div>
+        </div>
+	</div>
+	<div class="row">
+		<div class="sidebar1">
+            <div class="left-navigation">
+                <ul class="list">
+                    <li><a href=""> Billing Systems</a></li>
+                    <li><a href=""> Reporting Apps</a></li>
+                    <li><a href=""> Medicaid</a></li>
+                    <li><a href=""> HIPAA Security Mandate</a></li>
+                    <li><a href=""> Rental Tracking</a></li>
+                    <li><a href=""> ERN System</a></li>
+                </ul>
+                <ul class="list">
+                    <h5><strong>RELATED LINK</strong></h5>
+                    <li><a href=""> HL7 International</a></li>
+                    <li><a href=""> HIPAA Standard</a></li>
+                    <li><a href=""> Medicaid</a></li>
+                    <li><a href=""> ICD-9 Codes</a></li>
+                    <li><a href=""> DME</a></li>
+                    <li><a href=""> CMN</a></li>
+                    <li><a href=""> POS</a></li>
+                </ul>
+                <ul class="list">
+                    <h5><strong>NEWS / HEADLINES</strong></h5>
+                    <li><a href=""> HIPAA News</a></li>
+                    <li><a href=""> Medicaid News</a></li>
+                    <li><a href=""> ERN News</a></li>
+                    <li><a href=""> Sentracare New</a></li>
+                </ul>
+            </div>
+        </div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+<div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-2 main">
+	 <div class="content">
+		 <div class="details_header">
+			 <ul>
+				 <li><a href=""><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+				 <li><a href=""><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Client Area</a></li>
+				 <li><a href=""><span class="glyphicon glyphicon-book" aria-hidden="true"></span>Products</a></li>
+				 <li><a href=""><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>Services</a></li>
+				 <li><a href=""><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Contact & Supports</a></li>
+				 <li><a href=""><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>About Us</a></li>
+			 </ul>
+		</div>
+		<div class="company">
